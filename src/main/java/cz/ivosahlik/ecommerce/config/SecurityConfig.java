aad184bcd8ef7be48c3027172aa6b069f4f5a288
Ivo Vošahlík, External <ivosahlik@seznam.cz>
2024-08-24 14:24:34 +0200

    refactoring

--- src/main/java/cz/ivosahlik/ecommerce/config/SecurityConfig.java
+++ src/main/java/cz/ivosahlik/ecommerce/config/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests((requests)->requests
+                .authorizeHttpRequests(requests -> requests
                         .requestMatchers("/products").authenticated()
                         .requestMatchers("/auth/login").permitAll()
                         .anyRequest().permitAll())
